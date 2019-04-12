24555152f4429a885272c7060e2bfbd97b96da8d
Teo Klestrup Röijezon <teo@nullable.se>
2019-04-12 16:26:32 +0200

    Copy registry keys when deploying

--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,20 @@ export KUBECONFIG=deploy/kubeconfig
 
 export HELM_TLS_ENABLE=true
 export HELM_TLS_VERIFY=true
-export HELM_TLS_CA_CERT=deploy/tiller-certs/ca.crt
-export HELM_TLS_CERT=deploy/tiller-certs/tls.crt
-export HELM_TLS_KEY=deploy/tiller-certs/tls.key
+export HELM_TLS_CA_CERT=deploy/tiller-keys/ca.crt
+export HELM_TLS_CERT=deploy/tiller-keys/tls.crt
+export HELM_TLS_KEY=deploy/tiller-keys/tls.key
 
 mkdir -p deploy/tiller-certs
 kubectl get secret/tiller-secret --output=go-template='{{index .data "ca.crt"}}' | base64 -d > $HELM_TLS_CA_CERT
 kubectl get secret/tiller-secret --output=go-template='{{index .data "tls.crt"}}' | base64 -d > $HELM_TLS_CERT
 kubectl get secret/tiller-secret --output=go-template='{{index .data "tls.key"}}' | base64 -d > $HELM_TLS_KEY
+
+# Don't muck around with registry certs on non-CI machines, since Docker requires us to set it system-wide
+if [ "$CI" = 1 ]; then
+    sudo mkdir -p /etc/docker/certs.d
+    sudo cp --recursive deploy/registry-certs /etc/docker/certs.d/registry.kubernetes.etimo.se
+fi
 
 sbt docker:publish kubernetesHelmImageValues
 helm init --client-only
